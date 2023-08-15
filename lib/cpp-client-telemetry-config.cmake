473e995d7775d78f0f2cacd8fb687210e5ac370c
Rodrigo Varas <rodrigov@microsoft.com>
2023-08-14 17:21:01 -0700

    Add CURL dependency

--- lib/cpp-client-telemetry-config.cmake
+++ lib/cpp-client-telemetry-config.cmake
@@ -2,5 +2,6 @@ include(CMakeFindDependencyMacro)
 
 find_dependency(ZLIB)
 find_dependency(unofficial-sqlite3)
+find_dependency(CURL)
 
 include("${CMAKE_CURRENT_LIST_DIR}/cpp-client-telemetry-targets.cmake")
